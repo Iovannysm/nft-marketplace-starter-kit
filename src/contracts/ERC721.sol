3a2ad78b640cf9ecf6de25fac9728dae4aab40b2
Iovannysm <oenjad@gmail.com>
2022-12-10 20:40:08 -0800

    Created the ERC721Enumerable.sol file and added the functionality that when a token is mint it add a count of how many tokens have been minted so far.

--- src/contracts/ERC721.sol
+++ src/contracts/ERC721.sol
@@ -15,6 +15,26 @@ contract ERC721 {
   //Mapping from onwer to the number of owned tokens
   mapping(address => uint) private _OwnedTokenCount;
 
+    /// @notice Count all NFTs assigned to an owner
+    /// @dev NFTs assigned to the zero address are considered invalid, and this
+    ///  function throws for queries about the zero address.
+    /// @param _owner An address for whom to query the balance
+    /// @return The number of NFTs owned by `_owner`, possibly zero
+  function balanceOf(address _owner) public view returns(uint256){
+    require(_owner != address(0), 'owner query for non-existent token');
+    return _OwnedTokenCount[_owner];
+  }
+    /// @notice Find the owner of an NFT
+    /// @dev NFTs assigned to zero address are considered invalid, and queries
+    ///  about them do throw.
+    /// @param _tokenId The identifier for an NFT
+    /// @return The address of the owner of the NFT
+  function ownerOf(uint256 _tokenId) external view returns(address){
+    address owner = _tokenOwner[_tokenId];
+    require(owner != address(0), 'owner query for non-existent token');
+    return owner;
+  }
+
   function _exist(uint256 tokenId) internal view returns(bool){
     // setting the address of nft owner to check the mapping
     // of the address from tokenOwner at the tokenId 
@@ -23,7 +43,7 @@ contract ERC721 {
     return owner != address(0);
   }
 
-  function _mint(address to, uint256 tokenId) internal {
+  function _mint(address to, uint256 tokenId) internal virtual {
     // requires that the addres isn't zero
     require(to != address(0), 'ERC721: minting to the zero address');
     // requires thay the token doesn not already exist
